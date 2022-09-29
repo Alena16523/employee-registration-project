91682efc5b2272b7f7b7a4462f0443d055508428
Alena Elhajassaad <alenaelhajassaad@yahoo.com>
2022-09-28 23:37:06 -0400

    worked on some practice registration form

--- src/main/java/com/cydeo/model/Employee.java
+++ src/main/java/com/cydeo/model/Employee.java
@@ -1,7 +1,9 @@
 package com.cydeo.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -11,10 +13,27 @@ import java.time.LocalDate;
 @ToString
 public class Employee {
 
+   // @NotNull // field should not be null
+   // @NotBlank //field should not be blank "    " -> covers all of them
+    //@NotEmpty //field should not empty  ""
+
+    @NotBlank
+    @Size(max=12, min=2) //number of characters
+
     private String firstName;
     private String lastName;
+
+    @NotNull
+    //Thymeleaf accepts yyyy-MM-dd, but Local Date accepts mm-dd-yyyy
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
+    //@NotBlank
+    //@Email
     private String email;
+
+    //@NotBlank
+    //@Pattern(regexp = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{4,}")
     private String password;
     private String address;
     private String address2;
